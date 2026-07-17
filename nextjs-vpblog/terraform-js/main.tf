71b23eb28d6553f88d5d08da3882e5f86f8a6f2f
valiantprince <valiantbroussard@Valiants-MacBook-Pro-2.local>
2026-07-17 13:24:06 -0500

    Add S3 and CloudFront Terraform config for Next.js hosting

--- nextjs-vpblog/terraform-js/main.tf
+++ nextjs-vpblog/terraform-js/main.tf
@@ -54,3 +54,53 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
     ]
   })
 }
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.js portfolio site"
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id   = "S3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl             = 3600
+    max_ttl                 = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
